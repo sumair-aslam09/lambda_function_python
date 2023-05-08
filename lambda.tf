61186679f4c8294bb219276024f1d13ddf17067d
MOHAMMAD SUMAIR <91409244+sumair-aslam09@users.noreply.github.com>
2023-05-08 16:01:06 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -14,7 +14,7 @@ resource "aws_lambda_function" "test_lambda" {
   role          = "${aws_iam_role.lambda_role.arn}"
   handler       = "welcome.hello"
 
-  #source_code_hash = data.archive_file.welcome.output_base64sha256
+  source_code_hash = data.archive_file.welcome.output_base64sha256
 
   runtime = "python3.7"
 
