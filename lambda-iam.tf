637426ed79585b65425ed4250d4c04d2d0ffa893
MOHAMMAD SUMAIR <91409244+sumair-aslam09@users.noreply.github.com>
2023-05-08 14:28:05 +0530

    Update lambda-iam.tf

--- lambda-iam.tf
+++ lambda-iam.tf
@@ -2,8 +2,6 @@ resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_policy"
   role = aws_iam_role.lambda_role.id
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   policy = "${file("iam/lambda-policy.json")}"
 }
 
